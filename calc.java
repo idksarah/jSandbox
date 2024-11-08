0b63a54d1cec22b12eb676f7e797a7fab3870da9
idksarah <sarah.p.ngai@gmail.com>
2024-11-07 23:25:51 -0800

    cacl

---
+++ calc.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("pick an operation");
+
+        String operator = sc.nextLine();
+        double a,b;
+        double ans;
+        
+        if(operator.equals("+")){
+            System.out.println("enter your addens in the format a b");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            System.out.println("sum: " + (a + b));
+        } else if(operator.equals("-")){
+            System.out.println("enter your minuends and subtrahends in the format a b");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            System.out.println("dif: " + (a - b));
+        } else if(operator.equals("*")){
+            System.out.println("enter your products in the format a b");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            System.out.println("dif: " + (a * b));
+        } else if(operator.equals("/")){
+            System.out.println("enter your divisors and dividends in the format a b");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            System.out.println("dif: " + (a / b));
+        } else if(operator.equals("^")){
+            System.out.println("enter your base and power in the format a b");
+            a = sc.nextDouble();
+            ans = a;
+            b = sc.nextDouble();
+            for(double i = 1; i < b; i++){
+                ans = ans * a;
+            }
+            System.out.println(ans);
+        }
+    }
+}
